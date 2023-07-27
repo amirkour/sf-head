ff8b1aa96698ca093ebe46b3e1a082b4f8a54535
Amir Kouretchian <amir.kouretchian@symetra.com>
2023-07-27 11:02:09 -0700

    wip

--- projects/sf-head-web/src/lib/components/ItemList.tsx
+++ projects/sf-head-web/src/lib/components/ItemList.tsx
@@ -3,19 +3,25 @@ import { Item } from "../interfaces";
 export default function ItemList({ items }: { items: Item[] }) {
   return (
     <>
-      {items.length <= 0 && <div>No items</div>}
-      {items.length > 0 && (
-        <div className="flex flex-row flex-nowrap justify-evenly items-start">
-          <div>ID</div>
-          <div>NAME</div>
-        </div>
+      {items.length <= 0 ? (
+        <div>No items</div>
+      ) : (
+        <>
+          <div className="flex flex-row flex-nowrap justify-evenly items-start py-4">
+            <div>ID</div>
+            <div>NAME</div>
+          </div>
+          {items.map((item) => (
+            <div
+              key={item._id}
+              className="flex flex-row flex-nowrap justify-evenly items-start py-4"
+            >
+              <div>{item._id}</div>
+              <div>{item.name}</div>
+            </div>
+          ))}
+        </>
       )}
     </>
-    // items.map((item) => (
-    //   <div key={item._id} className="flex flex-row flex-nowrap justify-evenly items-start">
-    //     <div>{item._id}</div>
-    //     <div>{item.name}</div>
-    //   </div>
-    // ))}
   );
 }
